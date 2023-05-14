d28cf0ec3cc54c0c1024b187b27d1116a842a561
sarankumar0101 <119718662+sarankumar0101@users.noreply.github.com>
2023-05-14 19:39:57 +0530

    import java.util.*; import java.lang.*; import java.io.*; public class Sample {     public static void main(String args[])throws java.lang.Exception     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         while(t-->0)         {         boolean bo=false;         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         if((a+b<c) || (b+c<a) || (a+c<b))         {         bo=true;         }         if(bo)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }         }     } }

---
+++ Monopoly.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Sample
+{
+    public static void main(String args[])throws java.lang.Exception 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+        boolean bo=false;
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if((a+b<c) || (b+c<a) || (a+c<b))
+        {
+        bo=true;
+        }
+        if(bo)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        } 
+    }
+}
